4c68638ba32469dcd63f4dcb5593b0740d922af8
Lucas Labre <lucaslabre@gmail.com>
2022-01-15 22:51:29 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -9,15 +9,29 @@ function Listing() {
 
     const [pageNumber, setPageNumber] = useState(0);
 
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
+
     // FORMA CORRETA
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=1`)
-        .then(response => {
-            const data = response.data as MoviePage;
-            console.log(data);
-            setPageNumber(data.number);
-        });
-    }, []);
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPage(data);
+                // TESTES
+                // console.log(data);
+                // setPageNumber(data.number);
+            });
+    }, [pageNumber]);
 
     // FORMA ERRADA
     // axios.get(`${BASE_URL}/movies?size=12&page=1`)
@@ -26,28 +40,43 @@ function Listing() {
     //         setPageNumber(data.number);
     //     });
 
+
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
+
     return (
         <>
-            <p>{pageNumber}</p>
+            {/* <p>{pageNumber}</p> */}
             <Pagination />
 
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
+                    {page.content.map(movie =>  (
+                            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                                <MovieCard movie={movie} />
+                            </div>
+                        )
+                    )}
+                    {/* <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard movie={movie} />
                     </div>
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
+                        <MovieCard movie={movie} />
                     </div>
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
+                        <MovieCard movie={movie} />
                     </div>
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
+                        <MovieCard movie={movie} />
                     </div>
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
+                        <MovieCard movie={movie} />
+                    </div> */}
                 </div>
             </div>
 
